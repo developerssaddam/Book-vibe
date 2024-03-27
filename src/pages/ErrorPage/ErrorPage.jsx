bc5b3b1a323967a0241a2d9fb0af096628a1db03
developersaddam <developerssaddam@gmail.com>
2024-03-27 19:32:32 +0600

    Added 404 page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,18 @@
-
+import { Link } from "react-router-dom";
+import notFoundImg from "../../assets/img/404.png";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className="flex flex-col gap-5 justify-center items-center h-[100vh]">
+      <img src={notFoundImg} alt="" />
+      <Link
+        to="/"
+        className="bg-[#23BE0A] text-white text-2xl font-bold px-5 py-2 rounded-3xl"
+      >
+        Go-Back
+      </Link>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
